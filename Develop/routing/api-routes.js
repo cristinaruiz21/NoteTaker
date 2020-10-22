d29b4edc4057db1b7a78622a38e1606931bed3f1
Cristina Ruiz <cristinaruiz96@yahoo.com>
2020-10-21 22:36:01 -0500

    newNote var created

--- Develop/routing/api-routes.js
+++ Develop/routing/api-routes.js
@@ -23,14 +23,6 @@ module.exports = function(app) {
     res.json(note);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
   app.post("/api/notes", function(req, res) {
     // req.body is available since we're using the body parsing middleware
       note.push(req.body);
